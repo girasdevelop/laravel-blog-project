3fa1c57bc4904dc9c39b9bf0a5e28d2bcc546574
Andrey Girnik <girnikandrey@gmail.com>
2018-07-12 13:15:37 +0700

    Modify models, create base auth

--- app/Role.php
+++ app/Role.php
@@ -4,7 +4,46 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Role
+ * @package App
+ */
 class Role extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'slug', 'permissions',
+    ];
+    protected $casts = [
+        'permissions' => 'array',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'role_users');
+    }
+
+    /**
+     * @param array $permissions
+     * @return bool
+     */
+    public function hasAccess(array $permissions) : bool
+    {
+        foreach ($permissions as $permission) {
+            if ($this->hasPermission($permission))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $permission
+     * @return bool
+     */
+    private function hasPermission(string $permission) : bool
+    {
+        return $this->permissions[$permission] ?? false;
+    }
 }
--- app/User.php
+++ app/User.php
@@ -5,25 +5,54 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ * @package App
+ */
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_users');
+    }
+
+    /**
+     * Checks if User has access to $permissions.
+     * @param array $permissions
+     * @return bool
+     */
+    public function hasAccess(array $permissions) : bool
+    {
+        // check if the permission is available in any role
+        /* @var Role $role */
+        foreach ($this->roles as $role) {
+            if($role->hasAccess($permissions)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the user belongs to role.
+     * @param string $roleSlug
+     * @return bool
+     */
+    public function inRole(string $roleSlug)
+    {
+        return $this->roles()->where('slug', $roleSlug)->count() == 1;
+    }
 }
