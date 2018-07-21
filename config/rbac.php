5af691efb28adbccdfd0e0f396b00227a6ef55df
Andrey Girnik <girnikandrey@gmail.com>
2018-07-21 10:51:32 +0600

    Base install RBAC

--- config/rbac.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'userModelClass' => App\User::class,
-];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(\PermissionSeeder::class);
-        $this->call(\RoleSeeder::class);
     }
 }
